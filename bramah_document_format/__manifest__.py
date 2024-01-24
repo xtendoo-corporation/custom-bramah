3a1dc23c74cc70a36fcaba7e5341909ae8b40c97
dani <dani.xtendoo@gmail.com>
2024-01-24 12:18:58 +0100

    [ADD] bramah_document_format

---
+++ bramah_document_format/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Bramah Document Format",
+    "summary": """Formatos de documentos para Bramah""",
+    "version": "16.1.0.0",
+    "description": """Formatos de documentos para Bramah""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "http://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "stock",
+    ],
+    "data": [
+        "views/picking/stock_picking.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
