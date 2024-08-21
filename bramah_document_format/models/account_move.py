5e0a1d262e94d6e90c571d1b1d2e200bfbdc37ba
dani <dani.xtendoo@gmail.com>
2024-08-21 10:00:14 +0200

    añadidas nota del pedido en factura agrupada por picking

--- bramah_document_format/models/account_move.py
+++ bramah_document_format/models/account_move.py
@@ -2,16 +2,16 @@ import datetime
 from collections import OrderedDict
 
 from odoo import api, models
-from odoo.tools import float_is_zero
+from lxml import html
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
     def get_sale_order_note(self,sale_order_id):
-        sale_order = self.env['sale.order'].search([('id', '=', sale_order_id)])
-        print("*"*50)
-        print("ENTRA")
-        print("note", sale_order.note)
-        print("*"*50)
-        return sale_order.note
+        sale_order = self.env['sale.order'].search([('id', '=', sale_order_id)], limit=1)
+        if sale_order and sale_order.note:
+            note_text = html.fromstring(sale_order.note).text_content().strip()
+            if note_text:
+                return sale_order.note
+        return None
