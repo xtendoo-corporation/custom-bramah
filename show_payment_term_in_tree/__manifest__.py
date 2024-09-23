4b7349681488512f9ec152816cddf450af758671
dani <dani.xtendoo@gmail.com>
2024-09-23 10:56:14 +0200

    [ADD] show_payment_term_in_tree

---
+++ show_payment_term_in_tree/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Camilo Prado (https://xtendoo.es)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Importador de stock Bramah",
+    "version": "16.0",
+    "author": "Dani Domínguez(https://xtendoo.es)",
+    "category": "Bramah",
+    "license": "AGPL-3",
+    "depends": [
+        "contacts",
+    ],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    'installable': True,
+    'active': False,
+}
